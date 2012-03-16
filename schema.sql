c76b7021366c258152d59624389ae3fe4f9484d5
Björn Harrtell <bjorn@wololo.org>
2012-03-16 18:13:04 +0100

    New simple schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,18 @@
+-- SolidNet
+-- PostgreSQL schema for stable link entity networks
+-- requires PostGIS and uuid-ossp extension
+
+-- contains representations of Link
 CREATE TABLE links
 (
   id uuid NOT NULL DEFAULT uuid_generate_v1(),
-  created timestamp without time zone NOT NULL,
-  updated timestamp without time zone,
   CONSTRAINT reflinks_pkey PRIMARY KEY (id)
 );
-SELECT AddGeometryColumn('links', 'geom', 4326, 'LINESTRING', 2);
+-- store each Link geometry as simple linestring
+-- perhaps real topology one day with PostGIS 2.0+ ?
+SELECT AddGeometryColumn('links', 'geom', -1, 'LINESTRING', 2);
 
+-- contains representations of Node
 CREATE TABLE nodes
 (
   id uuid NOT NULL DEFAULT uuid_generate_v1(),
@@ -14,8 +20,10 @@ CREATE TABLE nodes
   ended timestamp without time zone,
   CONSTRAINT nodes_pkey PRIMARY KEY (id)
 );
-SELECT AddGeometryColumn('nodes', 'geom', 4326, 'POINT', 2);
+-- intentional redundant geometry for speed
+SELECT AddGeometryColumn('nodes', 'geom', -1, 'POINT', 2);
 
+-- contains representations of Linkport
 CREATE TABLE linkports
 (
   id uuid NOT NULL DEFAULT uuid_generate_v1(),
@@ -26,71 +34,52 @@ CREATE TABLE linkports
       REFERENCES links (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE
 );
-SELECT AddGeometryColumn('linkports', 'geom', 4326, 'POINT', 2);
+-- intentional redundant geometry for speed
+SELECT AddGeometryColumn('linkports', 'geom', -1, 'POINT', 2);
 
-CREATE TABLE nodeports
-(
-  id uuid NOT NULL DEFAULT uuid_generate_v1(),
-  node uuid NOT NULL,
-  CONSTRAINT nodeports_pkey PRIMARY KEY (id ),
-  CONSTRAINT nodeports_node_fkey FOREIGN KEY (node)
-      REFERENCES nodes (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE
-);
-SELECT AddGeometryColumn('nodeports', 'geom', 4326, 'POINT', 2);
-
+-- contains representations of Linkpart
 CREATE TABLE linkparts
 (
   id uuid NOT NULL DEFAULT uuid_generate_v1(),
-  starttime timestamp without time zone NOT NULL,
-  endtime timestamp without time zone,
-  startport uuid NOT NULL,
-  endport uuid NOT NULL,
+  starttime timestamp NOT NULL,
+  endtime timestamp,
+  fromlinkport uuid NOT NULL,
+  tolinkport uuid NOT NULL,
   CONSTRAINT linkparts_pkey PRIMARY KEY (id),
-  CONSTRAINT linkparts_endport_fkey FOREIGN KEY (endport)
+  CONSTRAINT linkparts_tolinkport_fkey FOREIGN KEY (tolinkport)
       REFERENCES linkports (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE,
-  CONSTRAINT linkparts_startport_fkey FOREIGN KEY (startport)
-      REFERENCES linkports (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE
-);
-SELECT AddGeometryColumn('linkparts', 'geom', 4326, 'LINESTRING', 2);
-
-CREATE TABLE nodeportsnodeports
-(
-  fromport uuid NOT NULL,
-  toport uuid NOT NULL,
-  CONSTRAINT nodeportsnodeports_pkey PRIMARY KEY (fromport, toport),
-  CONSTRAINT nodeportsnodeports_fromport_fkey FOREIGN KEY (fromport)
-      REFERENCES nodeports (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE,
-  CONSTRAINT nodeportnodeports_toport_fkey FOREIGN KEY (toport)
-      REFERENCES nodeports (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE
-);
-
-CREATE TABLE nodeportslinkports
-(
-  fromport uuid NOT NULL,
-  toport uuid NOT NULL,
-  CONSTRAINT nodeportslinkports_pkey PRIMARY KEY (fromport, toport),
-  CONSTRAINT nodeportslinkports_fromport_fkey FOREIGN KEY (fromport)
-      REFERENCES nodeports (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE,
-  CONSTRAINT nodeportsnodeports_toport_fkey FOREIGN KEY (toport)
+  CONSTRAINT linkparts_fromlinkport_fkey FOREIGN KEY (fromlinkport)
       REFERENCES linkports (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE
 );
+-- intentional redundant geometry for speed
+SELECT AddGeometryColumn('linkparts', 'geom', -1, 'LINESTRING', 2);
 
-CREATE TABLE linkportslinkports
+-- contains connections between Nodes
+CREATE TABLE nodesnodes
 (
-  fromport uuid NOT NULL,
-  toport uuid NOT NULL,
-  CONSTRAINT linkportslinkports_pkey PRIMARY KEY (fromport, toport),
-  CONSTRAINT linkportslinkports_fromport_fkey FOREIGN KEY (fromport)
-      REFERENCES linkports (id) MATCH SIMPLE
+  fromnode uuid NOT NULL,
+  tonode uuid NOT NULL,
+  CONSTRAINT nodesnodes_pkey PRIMARY KEY (fromnode, tonode),
+  CONSTRAINT nodesnodes_fromport_fkey FOREIGN KEY (fromnode)
+      REFERENCES nodes (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE,
-  CONSTRAINT nodeportsnodeports_toport_fkey FOREIGN KEY (toport)
+  CONSTRAINT nodesnodes_tonode_fkey FOREIGN KEY (tonode)
+      REFERENCES nodes (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+-- contains connections between Nodes and Linkports
+CREATE TABLE nodeslinkports
+(
+  node uuid NOT NULL,
+  linkport uuid NOT NULL,
+  CONSTRAINT nodeslinkports_pkey PRIMARY KEY (node, linkport),
+  CONSTRAINT nodeslinkports_node_fkey FOREIGN KEY (node)
+      REFERENCES nodes (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT nodeslinkports_linkport_fkey FOREIGN KEY (linkport)
       REFERENCES linkports (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE
 );
